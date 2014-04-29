9ee89282f14ff454583b86456e9b7cad51af7553
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-04-29 00:17:04 -0300

    Add Twitter Bootstrap module for testing Bootstrap 3.1.1 with JQuery 2.1.0.

---
+++ bootstrap/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * Created on 29/04/14, at 00:14.
+ */
+$(document).ready(function () {
+    console.log('JQuery configured successfully.');
+});
